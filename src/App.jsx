da4b1e476103ea2a54331f324037efe296f61192
AndreaBeltramin <andreabeltra99@gmail.com>
2025-03-21 12:35:08 +0100

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
+
+// funzione debounce generica definita fuori dal componente
+function debounce(callback, delay) {
+	let timer;
+	return (value) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			callback(value);
+		}, delay);
+	};
+}
 
 function App() {
 	const [search, setSearch] = useState("");
 	const [query, setQuery] = useState([]);
+
+	const fetchApi = useCallback(
+		debounce((search) => {
+			if (!search.trim()) {
+				setQuery([]);
+				return;
+			}
+			fetch(
+				`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${search}`
+			)
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+					return setQuery(data);
+				})
+				.catch((error) => console.error(error));
+		}, 300),
+		[]
+	);
+
 	useEffect(() => {
-		if (!search.trim()) {
-			setQuery([]);
-			return;
-		}
-		fetch(
-			`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${search}`
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-				return setQuery(data);
-			})
-			.catch((error) => console.error(error));
+		fetchApi(search);
 	}, [search]);
 
 	return (
 		<div>
-			<h1>Ricerca il prodotto:</h1>
+			<h1>Ricerca un prodotto:</h1>
 			<input
+				className="input"
 				type="text"
 				placeholder="Cerca un prodotto..."
 				value={search}
