1cb671ce5faaecdea15914d30a50633f36344bfa
AndreaBeltramin <andreabeltra99@gmail.com>
2025-03-21 11:02:26 +0100

    ricerca prodotto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+	const [search, setSearch] = useState("");
+	const [query, setQuery] = useState([]);
+	useEffect(() => {
+		fetch(
+			`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${search}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				return setQuery(data);
+			})
+			.catch((error) => console.error(error));
+	}, [search]);
+
 	return (
 		<div>
-			<h1>Titolo</h1>
+			<h1>Ricerca il prodotto:</h1>
+			<input
+				type="text"
+				placeholder="Cerca il prodotto"
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<div>
+				{query.map((product) => (
+					<div key={product.id}>
+						<h1>{product.name}</h1>
+						<p>{product.price}</p>
+						<p>{product.description}</p>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
